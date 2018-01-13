b8153e907bfa185339f5464e1d7ec34700086146
wuwei <wuwei@bytedance.com>
2018-01-13 10:34:36 +0800

    add form validate for appointment

--- src/containers/appointment.jsx
+++ src/containers/appointment.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { DatePicker } from 'antd';
+import { DatePicker, Form } from 'antd';
 import Button from 'components/button';
 import Select from 'components/select';
 import Input from 'components/input';
@@ -10,6 +10,16 @@ import moment from 'moment';
 import '../style/appointment.less';
 const dateFormat = 'YYYY/MM/DD';
 const { TextArea } = Input;
+const FormItem = Form.Item;
+
+const formItemLayout = {
+    labelCol: {
+        span: 3
+    },
+    wrapperCol: {
+        span: 21
+    }
+};
 
 function generateOptions(length, include) {
     const arr = [];
@@ -23,85 +33,106 @@ function generateOptions(length, include) {
 
 class Appointment extends Component {
     render () {
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="appointment-container">
                 <div className="appoint-left">
                     <div className="send-btn">Send</div>
                 </div>
                 <div className="appoint-main">
-                    <div className="item">
-                        <Select defaultValue="1" style={{ width: 85, marginRight: 8 }}>
-                            <Option value="1">From</Option>
-                        </Select>
-                        <Input placeholder="wuwei@meeting.com" disabled/>
-                    </div>
-                    <div className="item">
-                        <Button style={{ width: 85, marginRight: 8 }}>To...</Button>
-                        <Input placeholder="Basic usage" />
-                    </div>
-                    <div className="item">
-                        <div className="label">Subject</div><Input placeholder="Basic usage" />
-                    </div>
-                    <div className="item">
-                        <div className="label">Location</div><Input placeholder="Basic usage" /><div className="rooms" />
-                    </div>
-                    <div className="item">
-                        <div className="label">Start Time</div>
-                        <DatePicker
-                            format="YYYY-MM-DD"
-                            placeholder="Select Date"
-                            onChange={() => {}}
-                            onOk={() => {}}
-                            className="my-date-picker"
-                        />
-                        <TimePicker
-                            prefixCls="ant-time-picker"
-                            placeholder="Select Time"
-                            showSecond={false}
-                            defaultValue={moment()}
-                            hideDisabledOptions={true}
-                            disabledHours={(h) => {
-                                return [0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23];
-                            }}
-                            disabledMinutes={(m) => {
-                                return generateOptions(60, (m) => {
-                                    return m % 30 !== 0
-                                });
-                            }}
-                        />
-                    </div>
-                    <div className="item">
-                        <div className="label">End Time</div>
-                        <DatePicker
-                            format="YYYY-MM-DD"
-                            placeholder="Select Date"
-                            onChange={() => {}}
-                            onOk={() => {}}
-                            className="my-date-picker"
-                        />
-                        <TimePicker
-                            prefixCls="ant-time-picker"
-                            placeholder="Select Time"
-                            showSecond={false}
-                            defaultValue={moment()}
-                            hideDisabledOptions={true}
-                            disabledHours={(h) => {
-                                return [0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23];
-                            }}
-                            disabledMinutes={(m) => {
-                                return generateOptions(60, (m) => {
-                                    return m % 30 !== 0
-                                });
-                            }}
-                        />
-                    </div>
-                    <div className="item">
-                        <TextArea placeholder="Autosize height with minimum and maximum number of lines" autosize={{ minRows: 6}} />
-                    </div>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormItem
+                            label={<Select defaultValue="1" style={{ width: 85 }}>
+                                <Option value="1">From</Option>
+                            </Select>}
+                            {...formItemLayout}
+                        >
+                            <Input placeholder="wuwei@meeting.com" disabled/>
+                        </FormItem>
+                        <FormItem
+                            label={<Button style={{ width: 85 }}>To...</Button>}
+                            {...formItemLayout}
+                        >
+                            <Input />
+                        </FormItem>
+                        <FormItem
+                            label="Subject"
+                            {...formItemLayout}
+                        >
+                            <Input placeholder="Basic usage" />
+                        </FormItem>
+                        <FormItem
+                            label="Location"
+                            {...formItemLayout}
+                        >
+                            <div className="item">
+                                <Input placeholder="Basic usage" /><div className="rooms" />
+                            </div>
+                        </FormItem>
+                        <FormItem
+                            label="Start Time"
+                            {...formItemLayout}
+                        >
+                            <DatePicker
+                                format="YYYY-MM-DD"
+                                placeholder="Select Date"
+                                onChange={() => {}}
+                                onOk={() => {}}
+                                className="my-date-picker"
+                            />
+                            <TimePicker
+                                prefixCls="ant-time-picker"
+                                placeholder="Select Time"
+                                showSecond={false}
+                                defaultValue={moment()}
+                                hideDisabledOptions={true}
+                                disabledHours={(h) => {
+                                    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23];
+                                }}
+                                disabledMinutes={(m) => {
+                                    return generateOptions(60, (m) => {
+                                        return m % 30 !== 0
+                                    });
+                                }}
+                            />
+                        </FormItem>
+                        <FormItem
+                            label="End Time"
+                            {...formItemLayout}
+                        >
+                            <DatePicker
+                                format="YYYY-MM-DD"
+                                placeholder="Select Date"
+                                onChange={() => {}}
+                                onOk={() => {}}
+                                className="my-date-picker"
+                            />
+                            <TimePicker
+                                prefixCls="ant-time-picker"
+                                placeholder="Select Time"
+                                showSecond={false}
+                                defaultValue={moment()}
+                                hideDisabledOptions={true}
+                                disabledHours={(h) => {
+                                    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23];
+                                }}
+                                disabledMinutes={(m) => {
+                                    return generateOptions(60, (m) => {
+                                        return m % 30 !== 0
+                                    });
+                                }}
+                            />
+                        </FormItem>
+                        <div className="item">
+                            <TextArea placeholder="Autosize height with minimum and maximum number of lines" autosize={{ minRows: 6}} />
+                        </div>
+                    </Form>
                 </div>
             </div>
         )
     }
 }
 
-export default Appointment
+const WrappedDynamicRule = Form.create()(Appointment);
+
+export default WrappedDynamicRule
