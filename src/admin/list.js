757b5b72319f019c3ccf425b93bfa49c9c33c7a3
wuwei <dlutwuwei@qq.com>
2018-02-02 22:41:30 +0800

    fist list

--- src/admin/list.js
+++ src/admin/list.js
@@ -1,118 +1,178 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import moment from 'moment';
-import { List, Card, Row, Col, Radio, Input, Progress, Button, Icon, Dropdown, Menu, Avatar } from 'antd';
-
+import { Modal, Divider, List, Card, Form, Row, Col, Badge, Radio, Input, Progress, Button, Icon, Dropdown, Menu, Avatar } from 'antd';
+import fetch from 'lib/fetch';
+import StandardTable from 'components/standard-table';
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 const { Search } = Input;
 const styles = {};
-export default class BasicList extends PureComponent {
-  componentDidMount() {
-  }
+const statusMap = ['default', 'processing', 'success', 'error'];
+const status = ['关闭', '运行中', '已上线', '异常'];
+const columns = [
+    {
+        title: '姓名',
+        dataIndex: 'name',
+    },
+    {
+        title: '邮箱',
+        dataIndex: 'mail',
+    },
+    {
+        title: '联系方式',
+        dataIndex: 'contact'
+    },
+    {
+        title: '部门',
+        dataIndex: 'departmentName'
+    },
+    {
+        title: '地区',
+        dataIndex: 'cityName'
+    },
+    {
+        title: '操作',
+        render: () => (
+            <Fragment>
+                <a href=""><Icon type="form" /></a>
+                <Divider type="vertical" />
+                <a href=""><Icon type="delete" /></a>
+            </Fragment>
+        ),
+    },
+];
+const FormItem = Form.Item;
 
-  render() {
-    let { list, loading } = this.props;
-    list = []
-    const Info = ({ title, value, bordered }) => (
-      <div className={styles.headerInfo}>
-        <span>{title}</span>
-        <p>{value}</p>
-        {bordered && <em />}
-      </div>
-    );
-
-    const extraContent = (
-      <div className={styles.extraContent}>
-        <RadioGroup defaultValue="all">
-          <RadioButton value="all">全部</RadioButton>
-          <RadioButton value="progress">进行中</RadioButton>
-          <RadioButton value="waiting">等待中</RadioButton>
-        </RadioGroup>
-        <Search
-          className={styles.extraContentSearch}
-          placeholder="请输入"
-          onSearch={() => ({})}
-        />
-      </div>
-    );
-
-    const paginationProps = {
-      showSizeChanger: true,
-      showQuickJumper: true,
-      pageSize: 5,
-      total: 50,
+const CreateForm = Form.create()((props) => {
+    const { modalVisible, form, handleAdd, handleModalVisible } = props;
+    const okHandle = () => {
+        form.validateFields((err, fieldsValue) => {
+            if (err) return;
+            handleAdd(fieldsValue);
+        });
     };
-
-    const ListContent = ({ data: { owner, createdAt, percent, status } }) => (
-      <div className={styles.listContent}>
-        <div className={styles.listContentItem}>
-          <span>Owner</span>
-          <p>{owner}</p>
-        </div>
-        <div className={styles.listContentItem}>
-          <span>开始时间</span>
-          <p>{moment(createdAt).format('YYYY-MM-DD HH:mm')}</p>
-        </div>
-        <div className={styles.listContentItem}>
-          <Progress percent={percent} status={status} strokeWidth={6} style={{ width: 180 }} />
-        </div>
-      </div>
-    );
-
-    const menu = (
-      <Menu>
-        <Menu.Item>
-          <a>编辑</a>
-        </Menu.Item>
-        <Menu.Item>
-          <a>删除</a>
-        </Menu.Item>
-      </Menu>
-    );
-
-    const MoreBtn = () => (
-      <Dropdown overlay={menu}>
-        <a>
-          更多 <Icon type="down" />
-        </a>
-      </Dropdown>
-    );
-
     return (
-        <div className="">
-          <Card
-            className={styles.listCard}
-            bordered={false}
-            title="标准列表"
-            style={{ marginTop: 24 }}
-            bodyStyle={{ padding: '0 32px 40px 32px' }}
-            extra={extraContent}
-          >
-            <Button type="dashed" style={{ width: '100%', marginBottom: 8 }} icon="plus">
-              添加
-            </Button>
-            <List
-              size="large"
-              rowKey="id"
-              loading={loading}
-              pagination={paginationProps}
-              dataSource={list}
-              renderItem={item => (
-                <List.Item
-                  actions={[<a>编辑</a>, <MoreBtn />]}
-                >
-                  <List.Item.Meta
-                    avatar={<Avatar src={item.logo} shape="square" size="large" />}
-                    title={<a href={item.href}>{item.title}</a>}
-                    description={item.subDescription}
-                  />
-                  <ListContent data={item} />
-                </List.Item>
-              )}
-            />
-          </Card>
-        </div>
+        <Modal
+            title="新建规则"
+            visible={modalVisible}
+            onOk={okHandle}
+            onCancel={() => handleModalVisible()}
+        >
+            <FormItem
+                labelCol={{ span: 5 }}
+                wrapperCol={{ span: 15 }}
+                label="描述"
+            >
+                {form.getFieldDecorator('desc', {
+                    rules: [{ required: true, message: 'Please input some description...' }],
+                })(
+                    <Input placeholder="请输入" />
+                    )}
+            </FormItem>
+        </Modal>
     );
-  }
+});
+
+export default class BasicList extends PureComponent {
+    state = {
+        list: [],
+        selectedRows: [],
+        loading: false,
+        modalVisible: false
+    }
+    componentDidMount() {
+    }
+    fetchUsers = () => {
+        fetch.get('/api/user/getList', {
+            token: localStorage.getItem('__meeting_token')
+        }).then(res => {
+            this.setState({
+                list: res.data
+            });
+        });
+    }
+    componentWillReceiveProps(nextProps) {
+        const { match } = nextProps;
+        this.fetchUsers();
+    }
+    handleSelectRows = () => {
+
+    }
+    handleStandardTableChange = () => {
+
+    }
+    handleModalVisible = (flag) => {
+        this.setState({
+            modalVisible: !!flag,
+        });
+    }
+    handleAdd = (fields) => {
+        this.props.dispatch({
+            type: 'rule/add',
+            payload: {
+                description: fields.desc,
+            },
+        });
+
+        message.success('添加成功');
+        this.setState({
+            modalVisible: false,
+        });
+    }
+    render() {
+        const { list: data, selectedRows, loading, modalVisible } = this.state;
+
+        const paginationProps = {
+            showSizeChanger: true,
+            showQuickJumper: true,
+            pageSize: 5,
+            total: 50,
+        };
+        const parentMethods = {
+            handleAdd: this.handleAdd,
+            handleModalVisible: this.handleModalVisible,
+        };
+        const ListContent = ({ data: { owner, createdAt, percent, status } }) => (
+            <div className={styles.listContent}>
+                <div className={styles.listContentItem}>
+                    <span>Owner</span>
+                    <p>{owner}</p>
+                </div>
+                <div className={styles.listContentItem}>
+                    <span>开始时间</span>
+                    <p>{moment(createdAt).format('YYYY-MM-DD HH:mm')}</p>
+                </div>
+                <div className={styles.listContentItem}>
+                    <Progress statusMap={percent} status={status} strokeWidth={6} style={{ width: 180 }} />
+                </div>
+            </div>
+        );
+
+        return (
+            <div className="">
+                <Card
+                    className={styles.listCard}
+                    bordered={false}
+                    title="用户管理"
+                    style={{ marginTop: 24 }}
+                    bodyStyle={{ padding: '0 32px 40px 32px' }}
+                >
+                    <Button type="primary" style={{ marginTop: 8 }} icon="plus" onClick={() => this.handleModalVisible(true)}>添加</Button>
+                    <StandardTable
+                        selectedRows={selectedRows}
+                        loading={loading}
+                        data={data}
+                        columns={columns}
+                        onSelectRow={this.handleSelectRows}
+                        onChange={this.handleStandardTableChange}
+                    />
+                </Card>
+                <CreateForm
+                    {...parentMethods}
+                    modalVisible={modalVisible}
+                />
+            </div>
+        );
+    }
 }
