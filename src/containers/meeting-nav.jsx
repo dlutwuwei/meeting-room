fc1c747c22c4d951eb2f48034c8723a2633519bd
wuwei <wuwei@bytedance.com>
2018-03-12 11:05:15 +0800

    change showas

--- src/containers/meeting-nav.jsx
+++ src/containers/meeting-nav.jsx
@@ -67,11 +67,11 @@ class Nav extends Component {
                                 <Select defaultValue="1" style={{ width: 120 }} onChange={(val)=> {
                                     localStorage.setItem('__meeting_showas', val);
                                 }}>
-                                    <Option key="1" value="1" className="status busy" title="Busy">Busy</Option>
-                                    <Option key="2"  value="2" className="status out" title="Out of Office">Out of Office</Option>
-                                    <Option key="3" value="3" className="status interim" title="Tentative">Tentative</Option>
-                                    <Option key="3" value="4" className="status unkown" title="No Information">No Information</Option>
-                                    <Option key="3" value="5" className="status occupy" title="Working Elsewhere">Working Elsewhere</Option>
+                                    <Option key="1" value="2" className="status busy" title="Busy">Busy</Option>
+                                    <Option key="2"  value="3" className="status out" title="Out of Office">Out of Office</Option>
+                                    <Option key="3" value="1" className="status interim" title="Tentative">Tentative</Option>
+                                    <Option key="3" value="5" className="status unkown" title="No Information">No Information</Option>
+                                    <Option key="3" value="4" className="status occupy" title="Working Elsewhere">Working Elsewhere</Option>
                                 </Select>
                             </div>
                             <div className="nav-item1">
