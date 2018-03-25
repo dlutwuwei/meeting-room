03f67d8bc18fad1d98345bd7914e0c97ce0e7ab1
wuwei <dlutwuwei@qq.com>
2018-03-25 22:36:57 +0800

    fix

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -26,6 +26,7 @@ class Home extends Component {
       <div>
         <header className="app-header">
           <img src={logo} className="app-logo" alt="logo" />
+          <span style={{padding: '0 20px'}}>{localStorage.getItem('__meeting_user_name')}</span>
         </header>
         <div className="app-main card-container">
           <ul className="tabs">
