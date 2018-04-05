bb46ea300f0745ab6d041b1f2ddd48ba643c493e
yangjian.fe <yangjian.fe@bytedance.com>
2018-04-05 10:01:02 +0800

    页面重构
    Change-Id: I63279696edc1216e86b0bbb261110075cb155e4f

--- src/train-container/url.js
+++ src/train-container/url.js
@@ -5,4 +5,4 @@ export const train_delete = '/api/training/delete';
 export const train_item = '/api/training/item';
 
 export const train_book_list = '/api/training/getRoomScheduleList';
-export const train_brand_list = 'api/training/getBrandList';
+export const train_brand_list = '/api/training/getBrandList';
