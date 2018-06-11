7c1b01ab9eec3cc9363287d6a2d32f73d0197ca2
wuwei <dlutwuwei@qq.com>
2018-06-12 07:21:48 +0800

    fix img

--- webpack.config.js
+++ webpack.config.js
@@ -102,7 +102,7 @@ module.exports = Object.keys(languages).map(lan => {
               {
                 loader: 'url-loader',
                 options: {
-                  limit: 10000,
+                  limit: 1000,
                   name: 'img/[name].[ext]',
                 },
               },
